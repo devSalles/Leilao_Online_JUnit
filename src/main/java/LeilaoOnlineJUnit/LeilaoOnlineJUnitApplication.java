7d779685708262449fdd224050f49ae65ee7207d
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-08 20:28:34 -0300

    Primeiro commit: inicialização do projeto

---
+++ src/main/java/LeilaoOnlineJUnit/LeilaoOnlineJUnitApplication.java
@@ -0,0 +1,13 @@
+package LeilaoOnlineJUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoOnlineJUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaoOnlineJUnitApplication.class, args);
+	}
+
+}
