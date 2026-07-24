c6748963dcbf0b3d1a8561c18ebf2c5dc8307cb0
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-23 21:23:32 -0300

    infra: implementado tratamento de exceção global e inciado construção de classe de formatação de mensagem de exceção

--- src/main/java/LeilaoOnlineJUnit/infra/HandlerException.java
+++ src/main/java/LeilaoOnlineJUnit/infra/HandlerException.java
@@ -1,7 +1,20 @@
 package LeilaoOnlineJUnit.infra;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 
 @ControllerAdvice
 public class HandlerException {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<MessageRestError> ExcecoesGlobais()
+    {
+        MessageRestError messageRestError = new MessageRestError(HttpStatus.INTERNAL_SERVER_ERROR,"Erro interno, tente novamente mais tarde");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageRestError);
+    }
+
+
 }
--- src/main/java/LeilaoOnlineJUnit/infra/MessageRestError.java
+++ src/main/java/LeilaoOnlineJUnit/infra/MessageRestError.java
@@ -1,4 +1,26 @@
 package LeilaoOnlineJUnit.infra;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class MessageRestError {
+
+    private HttpStatus status;
+    private String mensagem;
+    private LocalDateTime dataHora;
+
+    MessageRestError(HttpStatus status, String mensagem)
+    {
+        this.status = status;
+        this.mensagem = mensagem;
+        this.dataHora = LocalDateTime.now();
+    }
+
 }
