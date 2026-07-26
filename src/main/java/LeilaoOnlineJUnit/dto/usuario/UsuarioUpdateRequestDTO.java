f1b61a116a73e5f4c039117fb6066fb90ad7fe11
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-26 17:32:21 -0300

    dto: criada e desenvolvida DTO de usuario para atualização de registro

--- src/main/java/LeilaoOnlineJUnit/dto/usuario/UsuarioUpdateRequestDTO.java
+++ src/main/java/LeilaoOnlineJUnit/dto/usuario/UsuarioUpdateRequestDTO.java
@@ -1,4 +1,24 @@
 package LeilaoOnlineJUnit.dto.usuario;
 
-public record UsuarioUpdateRequestDTO() {
+import LeilaoOnlineJUnit.entity.Usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioUpdateRequestDTO(
+        @NotBlank(message = "Nome de participante obrigatório") @Size(min = 3,message = "O nome deve ter no mínimo 3 letras")
+        @Pattern(regexp = "^[\\p{L}\\s]+$", message = "Nome deve conter apenas letras")
+        String nome,
+
+        @NotBlank(message = "Email de participante obrigatório") @Email(message = "Formato de email inválido")
+        String email
+) {
+
+    public void UpdateUsuario(Usuario usuario)
+    {
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+    }
 }
+
