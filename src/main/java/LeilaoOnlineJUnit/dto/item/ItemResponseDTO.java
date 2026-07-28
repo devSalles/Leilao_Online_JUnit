1bad3b45b411588c83d5d2ba3c073b25e78afae7
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-28 15:35:02 -0300

    dto item request e response: desenvolvido DTO de request e response com seus respectivos atributos

--- src/main/java/LeilaoOnlineJUnit/dto/item/ItemResponseDTO.java
+++ src/main/java/LeilaoOnlineJUnit/dto/item/ItemResponseDTO.java
@@ -1,4 +1,22 @@
 package LeilaoOnlineJUnit.dto.item;
 
-public record ItemResponseDTO() {
+import LeilaoOnlineJUnit.Enum.StatusItem;
+import LeilaoOnlineJUnit.entity.Item;
+
+import java.math.BigDecimal;
+
+public record ItemResponseDTO(
+        Long id,
+        String nomeItem,
+        String descricaoItem,
+        String categoriaItem,
+        BigDecimal valorInicialItem,
+        StatusItem statusItem,
+        Long proprietarioItemId
+) {
+    public ItemResponseDTO fromItem(Item item)
+    {
+        return  new ItemResponseDTO(item.getId(),item.getNome(),item.getDescricao(),item.getCategoria(),item.getValorInicial()
+        ,item.getStatusItem(),item.getProprietario().getId());
+    }
 }
--- src/main/java/LeilaoOnlineJUnit/dto/item/ItemResquestDTO.java
+++ src/main/java/LeilaoOnlineJUnit/dto/item/ItemResquestDTO.java
@@ -1,4 +1,46 @@
 package LeilaoOnlineJUnit.dto.item;
 
-public record ItemResquestDTO() {
+import LeilaoOnlineJUnit.Enum.StatusItem;
+import LeilaoOnlineJUnit.entity.Item;
+import LeilaoOnlineJUnit.entity.Usuario;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ItemResquestDTO(
+
+        @NotBlank(message = "Nome de item obrigatório")
+        String nomeItem,
+
+        @NotBlank(message = "Descrição do item obrigatória")
+        String descricaoItem,
+
+        @NotBlank(message = "Categoria do item obrigatório")
+        String categoriaItem,
+
+        @NotNull(message = "Valor obrigatório") @Positive(message = "O valor deve ser maior que zero")
+        BigDecimal valorInicialItem,
+
+        @Enumerated(EnumType.STRING)
+        StatusItem statusItem,
+
+        @NotNull(message = "Id de proprietário de item obrigatório")
+        Long proprietarioItemId
+) {
+        public Item toItem(Usuario proprietario) {
+            Item item = new Item();
+
+            item.setNome(nomeItem);
+            item.setDescricao(descricaoItem);
+            item.setCategoria(categoriaItem);
+            item.setValorInicial(valorInicialItem);
+            item.setStatusItem(statusItem);
+            item.setProprietario(proprietario);
+
+            return item;
+        }
 }
