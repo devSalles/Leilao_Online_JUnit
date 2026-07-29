2c6b64b380b60f95be28fefad715915e4ab41f9b
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-29 17:24:48 -0300

    new files: realizada a criação de uma classe DTO para requisição de atualização de item e criação de exceç~~ao de item em leilão

---
+++ src/main/java/LeilaoOnlineJUnit/infra/exception/item/ItemEmLeilaoException.java
@@ -0,0 +1,7 @@
+package LeilaoOnlineJUnit.infra.exception.item;
+
+public class ItemEmLeilaoException extends RuntimeException {
+  public ItemEmLeilaoException(String message) {
+    super(message);
+  }
+}
