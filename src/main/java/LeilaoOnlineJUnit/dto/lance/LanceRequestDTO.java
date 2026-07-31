5b3b800921099b5b3b820c9ef3d5cc0bee82e76d
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-31 16:55:16 -0300

    dto lance request: desenvolvido DTO de request de lance

--- src/main/java/LeilaoOnlineJUnit/dto/lance/LanceRequestDTO.java
+++ src/main/java/LeilaoOnlineJUnit/dto/lance/LanceRequestDTO.java
@@ -1,4 +1,38 @@
 package LeilaoOnlineJUnit.dto.lance;
 
-public record LanceRequestDTO() {
+import LeilaoOnlineJUnit.entity.Lance;
+import LeilaoOnlineJUnit.entity.Leilao;
+import LeilaoOnlineJUnit.entity.Usuario;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record LanceRequestDTO(
+
+    @NotNull(message = "Valor de lance obrigatório")
+    @DecimalMin(value = "0.01",message = "Valor deve ser maior que zero")
+    @Digits(integer = 10,fraction = 2,message = "O valor deve ter no máximo 10 digitos inteiros e 2 decimais")
+    BigDecimal valorLance,
+
+    @NotNull(message = "O ID do usuário é obrigatório")
+    Long idUsuario,
+
+    @NotNull(message = "O ID leilão é obrigatório")
+    Long idLeilao
+) {
+
+    public Lance toLance(Usuario usuario, Leilao leilao)
+    {
+        Lance lance = new Lance();
+
+        lance.setValor(valorLance);
+        lance.setDataHora(LocalDateTime.now());
+        lance.setUsuario(usuario);
+        lance.setLeilao(leilao);
+
+        return lance;
+    }
 }
