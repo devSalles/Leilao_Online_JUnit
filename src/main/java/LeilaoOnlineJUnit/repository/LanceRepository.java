01018871e53cafc068847c3d764eaace37fbcb88
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-31 16:57:04 -0300

    lance repository e lance service: desenvolvido metodo de pesquisa para ordenação e pesquisa de maior valor + início de desenvolvimento de metodo para validação de lance

--- src/main/java/LeilaoOnlineJUnit/repository/LanceRepository.java
+++ src/main/java/LeilaoOnlineJUnit/repository/LanceRepository.java
@@ -1,9 +1,14 @@
 package LeilaoOnlineJUnit.repository;
 
 import LeilaoOnlineJUnit.entity.Lance;
+import LeilaoOnlineJUnit.entity.Leilao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LanceRepository extends JpaRepository<Lance,Long> {
+
+    Optional<Lance> findFirstByLeilaoOrderByValorDesc(Leilao  leilao);
 }
--- src/main/java/LeilaoOnlineJUnit/service/LanceService.java
+++ src/main/java/LeilaoOnlineJUnit/service/LanceService.java
@@ -1,13 +1,22 @@
 package LeilaoOnlineJUnit.service;
 
-import LeilaoOnlineJUnit.repository.ItemRepository;
+
+import LeilaoOnlineJUnit.entity.Lance;
+import LeilaoOnlineJUnit.entity.Leilao;
 import LeilaoOnlineJUnit.repository.LanceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class LanceService {
 
     private final LanceRepository lanceRepository;
+
+    private void validarValorLance(BigDecimal valorLance, Leilao leilao)
+    {
+
+    }
 }
