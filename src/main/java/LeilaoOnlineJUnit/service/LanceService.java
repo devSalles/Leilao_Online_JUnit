ee28421462db6a09a0fa453b53000d05f4260b6b
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-07-31 21:54:11 -0300

    lance service: criado metodos de validação de lance

--- src/main/java/LeilaoOnlineJUnit/service/LanceService.java
+++ src/main/java/LeilaoOnlineJUnit/service/LanceService.java
@@ -1,22 +1,73 @@
 package LeilaoOnlineJUnit.service;
 
 
+import LeilaoOnlineJUnit.Enum.StatusLeilao;
+import LeilaoOnlineJUnit.Enum.StatusUsuario;
+import LeilaoOnlineJUnit.dto.lance.LanceRequestDTO;
+import LeilaoOnlineJUnit.dto.lance.LanceResponseDTO;
 import LeilaoOnlineJUnit.entity.Lance;
 import LeilaoOnlineJUnit.entity.Leilao;
+import LeilaoOnlineJUnit.entity.Usuario;
+import LeilaoOnlineJUnit.infra.exception.item.LeilaoNaoAbertoException;
+import LeilaoOnlineJUnit.infra.exception.item.PrimeiroLanceInvaidoException;
+import LeilaoOnlineJUnit.infra.exception.participante.UsuarioBloqueadoException;
 import LeilaoOnlineJUnit.repository.LanceRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LanceService {
 
     private final LanceRepository lanceRepository;
+    private final UsuarioService usuarioService;
+    private final LeilaoService leilaoService;
+
+//    @Transactional
+//    public LanceResponseDTO realizarLance(LanceRequestDTO lanceRequestDTO)
+//    {
+//        Usuario participante = usuarioService.buscarIdUsuario(lanceRequestDTO.idUsuario());
+//        Leilao leilao = leilaoService;
+//    }
+
+    //--- Metodos Auxiliares ---
+
+    private void validarLeilao(Leilao leilao)
+    {
+        if(leilao.getStatusLeilao() != StatusLeilao.ABERTO)
+        {
+            throw new LeilaoNaoAbertoException();
+        }
+    }
+
+    private void validarUsuario(Usuario usuario)
+    {
+        if(usuario.getStatusUsuario() != StatusUsuario.ATIVO)
+        {
+            throw  new UsuarioBloqueadoException("Usuário bloqueado não pode realizar novos lances");
+        }
+    }
 
     private void validarValorLance(BigDecimal valorLance, Leilao leilao)
     {
+        Optional<Lance> maiorLance = lanceRepository.findFirstByLeilaoOrderByValorDesc(leilao);
 
+        if(maiorLance.isEmpty())
+        {
+            if(valorLance.compareTo(leilao.getItem().getValorInicial())<0)
+            {
+                throw new PrimeiroLanceInvaidoException();
+            }
+
+            return;
+        }
+
+        if(valorLance.compareTo(leilao.getItem().getValorInicial())<=0)
+        {
+            throw new PrimeiroLanceInvaidoException();
+        }
     }
 }
