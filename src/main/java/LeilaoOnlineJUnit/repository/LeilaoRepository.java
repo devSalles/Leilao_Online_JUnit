3339fc30be78c2edb235b7f3c260a9efaadae51e
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-03 16:10:20 -0300

    repository leilão: desenvolvido metodo para verificar se algum item já está vínculado ao leilão

--- src/main/java/LeilaoOnlineJUnit/repository/LeilaoRepository.java
+++ src/main/java/LeilaoOnlineJUnit/repository/LeilaoRepository.java
@@ -4,6 +4,8 @@ import LeilaoOnlineJUnit.Enum.StatusLeilao;
 import LeilaoOnlineJUnit.entity.Leilao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 import java.util.List;
 
 @Repository
@@ -11,4 +13,5 @@ public interface LeilaoRepository extends JpaRepository<Leilao,Long> {
 
     boolean existsByCriadorIdAndStatusLeilaoIn(Long criadorId, List<StatusLeilao> statusLeilao);
     boolean existsByItemId(Long itemId);
+    boolean existsByItemIdAndStatusLeilaoIn(Long idItem, Collection<StatusLeilao> statusLeilao);
 }
