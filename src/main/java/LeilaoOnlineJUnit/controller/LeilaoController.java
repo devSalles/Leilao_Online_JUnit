9bdd621d539d26a356df889f638e65ab24e65abc
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-03 16:17:09 -0300

    feat leilão service: adicionado funcionalidade de agendar leilão e adicionado endpoint na controller

--- src/main/java/LeilaoOnlineJUnit/controller/LeilaoController.java
+++ src/main/java/LeilaoOnlineJUnit/controller/LeilaoController.java
@@ -1,15 +1,34 @@
 package LeilaoOnlineJUnit.controller;
 
+import LeilaoOnlineJUnit.dto.leilao.LeilaoRequestDTO;
+import LeilaoOnlineJUnit.dto.leilao.LeilaoResponseDTO;
 import LeilaoOnlineJUnit.service.ItemService;
 import LeilaoOnlineJUnit.service.LeilaoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping(name = "/leilao")
+@RequestMapping("/leilao")
 @RequiredArgsConstructor
+@Tag(name = "Leilão")
 public class LeilaoController {
 
     private final LeilaoService leilaoService;
+
+    @PostMapping("/agendar-leilão")
+    public ResponseEntity<LeilaoResponseDTO> agendarLeilaoController(@Valid @RequestBody LeilaoRequestDTO leilaoRequestDTO)
+    {
+        LeilaoResponseDTO leilaoResponseDTO = leilaoService.agendarLeilao(leilaoRequestDTO);
+        URI creation = URI.create("/leilao/" + leilaoResponseDTO.id());
+        return ResponseEntity.created(creation).body(leilaoResponseDTO);
+    }
+
 }
--- src/main/java/LeilaoOnlineJUnit/service/LeilaoService.java
+++ src/main/java/LeilaoOnlineJUnit/service/LeilaoService.java
@@ -1,13 +1,86 @@
 package LeilaoOnlineJUnit.service;
 
-import LeilaoOnlineJUnit.repository.ItemRepository;
+import LeilaoOnlineJUnit.Enum.StatusItem;
+import LeilaoOnlineJUnit.Enum.StatusLeilao;
+import LeilaoOnlineJUnit.Enum.StatusUsuario;
+import LeilaoOnlineJUnit.dto.leilao.LeilaoRequestDTO;
+import LeilaoOnlineJUnit.dto.leilao.LeilaoResponseDTO;
+import LeilaoOnlineJUnit.entity.Item;
+import LeilaoOnlineJUnit.entity.Leilao;
+import LeilaoOnlineJUnit.entity.Usuario;
+import LeilaoOnlineJUnit.infra.exception.leilao.DataIncorretaException;
+import LeilaoOnlineJUnit.infra.exception.item.ItemEmLeilaoException;
+import LeilaoOnlineJUnit.infra.exception.item.ItemVendidoException;
+import LeilaoOnlineJUnit.infra.exception.item.ItemVinculadoAoLeilaoException;
+import LeilaoOnlineJUnit.infra.exception.participante.UsuarioBloqueadoException;
 import LeilaoOnlineJUnit.repository.LeilaoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LeilaoService {
 
     private final LeilaoRepository leilaoRepository;
+    private final ItemService itemService;
+    private final UsuarioService usuarioService;
+
+    public LeilaoResponseDTO agendarLeilao(LeilaoRequestDTO  leilaoRequestDTO)
+    {
+        Usuario criadorID = usuarioService.buscarIdUsuario(leilaoRequestDTO.idCriador());
+        Item itemID = itemService.buscarID(leilaoRequestDTO.idItem());
+
+        validarAgendamentoLeilao(criadorID, itemID);
+        validarDatasAgendamentoLeilao(leilaoRequestDTO);
+
+        boolean itemVinculadoLeilao = leilaoRepository.existsByItemIdAndStatusLeilaoIn(
+                itemID.getId(), List.of(StatusLeilao.ABERTO, StatusLeilao.AGENDADO));
+        if(itemVinculadoLeilao)
+        {
+            throw new ItemVinculadoAoLeilaoException("Esse item já esta vínculado ao leilão");
+        }
+
+        Leilao leilaoSalvar = leilaoRequestDTO.toLeilao(itemID,criadorID);
+
+        this.leilaoRepository.save(leilaoSalvar);
+        return LeilaoResponseDTO.fromLeilao(leilaoSalvar);
+    }
+
+    //--- Metodos Auxiliares ---
+
+    public void validarDatasAgendamentoLeilao(LeilaoRequestDTO  leilaoRequestDTO)
+    {
+        if (leilaoRequestDTO.dataInicio().isAfter(leilaoRequestDTO.dataFim())) {
+            throw new DataIncorretaException();
+        }
+
+        if (!leilaoRequestDTO.dataInicio().isAfter(LocalDateTime.now())) {
+            throw new DataIncorretaException("A data de início está incorreta");
+        }
+
+        if (!leilaoRequestDTO.dataFim().isAfter(leilaoRequestDTO.dataInicio())) {
+            throw new DataIncorretaException("A data de encerramento está incorreta");
+        }
+    }
+
+    public void validarAgendamentoLeilao(Usuario criador, Item item)
+    {
+        if(criador.getStatusUsuario().equals(StatusUsuario.BLOQUEADO))
+        {
+            throw new UsuarioBloqueadoException("usuario bloqueado não pode fazer agendamento de leilão");
+        }
+
+        if(item.getStatusItem().equals(StatusItem.EM_LEILAO))
+        {
+            throw new ItemEmLeilaoException("Item em leilão não pode ser vínculado");
+        }
+
+        if(item.getStatusItem().equals(StatusItem.VENDIDO))
+        {
+            throw new ItemVendidoException("Item vendido não pode ser vínculado");
+        }
+    }
 }
