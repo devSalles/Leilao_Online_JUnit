5083b2b133a165f722dc0b76eb489a165c51e4ba
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-04 12:55:42 -0300

    repository leilão: adicionado metodos de busca entre datas iniciais e finais, por status de leilão, criador e vencedor de leilão

--- src/main/java/LeilaoOnlineJUnit/repository/LeilaoRepository.java
+++ src/main/java/LeilaoOnlineJUnit/repository/LeilaoRepository.java
@@ -5,6 +5,7 @@ import LeilaoOnlineJUnit.entity.Leilao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,4 +15,10 @@ public interface LeilaoRepository extends JpaRepository<Leilao,Long> {
     boolean existsByCriadorIdAndStatusLeilaoIn(Long criadorId, List<StatusLeilao> statusLeilao);
     boolean existsByItemId(Long itemId);
     boolean existsByItemIdAndStatusLeilaoIn(Long idItem, Collection<StatusLeilao> statusLeilao);
+
+    List<Leilao> findByStatusLeilao(StatusLeilao statusLeilao);
+    List<Leilao> findByDataInicioBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+    List<Leilao> findByDataFimBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+    List<Leilao> findByCriadorId(Long criadorId);
+    List<Leilao> findByVencedorId(Long vencedorId);
 }
