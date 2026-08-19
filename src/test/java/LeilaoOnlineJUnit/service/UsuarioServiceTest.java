2604ed6caa0abf34a15ba647c74f9b87324b8909
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-19 17:26:26 -0300

    usuario service test: desenvolvido metodo de teste de atualização de usuário e teste de lançamento de exceção caso email repetido

--- src/test/java/LeilaoOnlineJUnit/service/UsuarioServiceTest.java
+++ src/test/java/LeilaoOnlineJUnit/service/UsuarioServiceTest.java
@@ -1,7 +1,10 @@
 package LeilaoOnlineJUnit.service;
 
 import LeilaoOnlineJUnit.dto.usuario.UsuarioRequestDTO;
+import LeilaoOnlineJUnit.dto.usuario.UsuarioResponseDTO;
+import LeilaoOnlineJUnit.dto.usuario.UsuarioUpdateRequestDTO;
 import LeilaoOnlineJUnit.entity.Usuario;
+import LeilaoOnlineJUnit.factory.UsuarioFactory;
 import LeilaoOnlineJUnit.infra.exception.participante.CpfRepetidoException;
 import LeilaoOnlineJUnit.infra.exception.participante.EmailRepetidoException;
 import LeilaoOnlineJUnit.repository.ItemRepository;
@@ -12,6 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -37,9 +44,16 @@ public class UsuarioServiceTest {
     @Test
     void deveSalvarUsuario()
     {
+        //Arrange
         UsuarioRequestDTO usuarioRequestDTO = new UsuarioRequestDTO("Bernardo","bernardo@gmail.com","142.829.436-88");
 
-        usuarioService.salvarUsuario(usuarioRequestDTO);
+        //Act
+        UsuarioResponseDTO usuarioResponse = usuarioService.salvarUsuario(usuarioRequestDTO);
+
+        //Assert
+        assertEquals("Bernardo",usuarioResponse.nome());
+        assertEquals("bernardo@gmail.com",usuarioResponse.email());
+        assertEquals("14282943688",usuarioResponse.cpf());
 
         usuarioRepository.save(any(Usuario.class));
     }
@@ -50,7 +64,7 @@ public class UsuarioServiceTest {
         //Arrange
         UsuarioRequestDTO usuarioRequestDTO = new UsuarioRequestDTO("Bernardo","bernardo@gmail.com","142.829.436-88");
 
-        when(usuarioRepository.existsByEmail("bernardo@gmail.com")).thenReturn(true);
+        when(usuarioRepository.existsByEmail(usuarioRequestDTO.email())).thenReturn(true);
 
         //Act
         assertThrows(EmailRepetidoException.class,()->usuarioService.salvarUsuario(usuarioRequestDTO));
@@ -62,12 +76,61 @@ public class UsuarioServiceTest {
     @Test
     void deveLancarExcecaoQuandoCpfExistente()
     {
+        //Arrange
         UsuarioRequestDTO usuarioRequestDTO = new UsuarioRequestDTO("Bernardo","bernardo@gmail.com","142.829.436-88");
 
+        //Act
         when(usuarioRepository.existsByCpf("14282943688")).thenReturn(true);
 
+        //Assert
         assertThrows(CpfRepetidoException.class,()->usuarioService.salvarUsuario(usuarioRequestDTO));
 
         verify(usuarioRepository,never()).save(any(Usuario.class));
     }
+
+    //--- PUT Usuario ---
+
+    @Test
+    void atualizarUsuario()
+    {
+        //Arrange
+        Usuario usuario = UsuarioFactory.criarUsuarioPronto();
+
+        UsuarioUpdateRequestDTO usuarioUpdtRequestDTO = new UsuarioUpdateRequestDTO("Bernardo","bernardo@gmail.com");
+
+        when(usuarioRepository.findById(usuario.getId())).thenReturn(Optional.of(usuario));
+        when(usuarioRepository.existsByEmailAndIdNot(usuarioUpdtRequestDTO.email(), usuario.getId())).thenReturn(false);
+
+        //Act
+        UsuarioResponseDTO usuarioResponse = usuarioService.atualizarUsuario(usuario.getId(),usuarioUpdtRequestDTO);
+
+        //Assert
+        assertEquals("Bernardo",usuarioResponse.nome());
+        assertEquals("bernardo@gmail.com",usuarioResponse.email());
+
+        verify(usuarioRepository,times(1)).findById(usuario.getId());
+        verify(usuarioRepository).save(any(Usuario.class));
+    }
+
+    @Test
+    void lancarExcecaoQuandoEmailRepetido()
+    {
+        //Arrange
+        Usuario usuario = UsuarioFactory.criarUsuarioPronto();
+        UsuarioUpdateRequestDTO usuarioUpdateRequestDTO = new UsuarioUpdateRequestDTO("Bernardo","bernardo@gmail.com");
+
+        when(usuarioRepository.findById(usuario.getId())).thenReturn(Optional.of(usuario));
+        when(usuarioRepository.existsByEmailAndIdNot(usuarioUpdateRequestDTO.email(), usuario.getId())).thenReturn(true);
+
+        //Act
+        EmailRepetidoException emailRepetidoException = assertThrows(EmailRepetidoException.class,()-> usuarioService.atualizarUsuario(usuario.getId(), usuarioUpdateRequestDTO));
+
+        //Assert
+        assertEquals("Email já cadastrado",emailRepetidoException.getMessage());
+
+        verify(usuarioRepository,never()).save(any(Usuario.class));
+        verify(usuarioRepository).findById(usuario.getId());
+    }
+
+
 }
