17550565f3d7c5a2961ceca5a12349a7951d1f80
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-23 17:25:59 -0300

    item service test: iniciado implementação de mocks e realizado a injeção de mock

--- src/test/java/LeilaoOnlineJUnit/service/ItemServiceTest.java
+++ src/test/java/LeilaoOnlineJUnit/service/ItemServiceTest.java
@@ -1,4 +1,24 @@
 package LeilaoOnlineJUnit.service;
 
+import LeilaoOnlineJUnit.repository.ItemRepository;
+import LeilaoOnlineJUnit.repository.LeilaoRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
+
+    @Mock
+    UsuarioService usuarioService;
+
+    @Mock
+    ItemRepository itemRepository;
+
+    @Mock
+    LeilaoRepository leilaoRepository;
+
+    @InjectMocks
+    ItemService itemService;
 }
