1c744ae52f60d3d95440156720d2c058f651793a
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-08-25 16:39:08 -0300

    item service test: Desenvolvido metodo de atualizar item e adicionado teste de lançamento de exceção caso tente atualizar item com status de leilão ou vendido e lançamento de exceção caso ID de item não seja encontrado

--- src/test/java/LeilaoOnlineJUnit/service/ItemServiceTest.java
+++ src/test/java/LeilaoOnlineJUnit/service/ItemServiceTest.java
@@ -1,12 +1,16 @@
 package LeilaoOnlineJUnit.service;
 
+import LeilaoOnlineJUnit.Enum.StatusItem;
 import LeilaoOnlineJUnit.dto.item.ItemResponseDTO;
 import LeilaoOnlineJUnit.dto.item.ItemResquestDTO;
 import LeilaoOnlineJUnit.dto.item.ItemUpdateRequestDTO;
 import LeilaoOnlineJUnit.entity.Item;
 import LeilaoOnlineJUnit.entity.Usuario;
+import LeilaoOnlineJUnit.factory.ItemFactory;
 import LeilaoOnlineJUnit.factory.UsuarioFactory;
 import LeilaoOnlineJUnit.infra.exception.IdNaoEncontradoException;
+import LeilaoOnlineJUnit.infra.exception.ItemEmLeilaoException;
+import LeilaoOnlineJUnit.infra.exception.ItemVendidoException;
 import LeilaoOnlineJUnit.repository.ItemRepository;
 import LeilaoOnlineJUnit.repository.LeilaoRepository;
 import org.junit.jupiter.api.Test;
@@ -17,6 +21,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -42,6 +47,7 @@ public class ItemServiceTest {
     @Test
     void registrarItens()
     {
+        // Arrange
         Usuario proprietario = UsuarioFactory.criarUsuarioPronto();
 
         when(usuarioService.buscarIdUsuario(proprietario.getId())).thenReturn(proprietario);
@@ -49,7 +55,10 @@ public class ItemServiceTest {
         ItemResquestDTO itemRequest = new ItemResquestDTO("Bicicleta","Excelente estado",
                 "veíclos",new BigDecimal("2500.00"), proprietario.getId());
 
+        //Act
         ItemResponseDTO itemResponseDTO = itemService.salvarItem(itemRequest);
+
+        //Assert
         assertNotNull(itemResponseDTO);
 
         verify(usuarioService).buscarIdUsuario(proprietario.getId());
@@ -68,6 +77,7 @@ public class ItemServiceTest {
     @Test
     void lancarExcecaoQuandoPropritarioNaoExistir()
     {
+        //Arrange
         Long idProprietario = 100L;
 
         when(usuarioService.buscarIdUsuario(idProprietario)).thenThrow(new IdNaoEncontradoException("Usuário não encontrado"));
@@ -75,12 +85,110 @@ public class ItemServiceTest {
         ItemResquestDTO itemRequest = new ItemResquestDTO("Bicicleta","Excelente estado",
                 "veíclos",new BigDecimal("2500.00"), idProprietario);
 
+        //Act
         IdNaoEncontradoException exception = assertThrows(IdNaoEncontradoException.class, () ->  itemService.salvarItem(itemRequest));
+
+        //Assert
         assertEquals("Usuário não encontrado",exception.getMessage());
 
         verify(usuarioService).buscarIdUsuario(idProprietario);
         verify(itemRepository,never()).save(any(Item.class));
     }
+
+    // --- PUT ITEM ---
+
+    @Test
+    void atualizarItem()
+    {
+        //Arrange
+        Usuario proprietario = UsuarioFactory.criarUsuarioPronto();
+        Item item = ItemFactory.criarItemPronto(proprietario);
+
+        when(itemRepository.findById(item.getId())).thenReturn(Optional.of(item));
+
+        ItemUpdateRequestDTO itemUpdateDTO = new ItemUpdateRequestDTO("Carro","perfeito estado",
+                "Veículo",new BigDecimal("20.00"));
+
+        //Act
+        ItemResponseDTO responde = itemService.atualizarItem(item.getId(),itemUpdateDTO);
+
+        //Assert
+        validarDadosItem(item,responde);
+
+        verify(itemRepository).save(any(Item.class));
+        verify(itemRepository).findById(item.getId());
+    }
+
+    @Test
+    void lancarExcecaoCasoItemEstiverEmLeilao()
+    {
+        //Arrange
+        Usuario proprietario = UsuarioFactory.criarUsuarioPronto();
+        Item item = ItemFactory.criarItemPersonalizado(1L,"Iphone","perfeito estado","telefone"
+                ,StatusItem.EM_LEILAO ,proprietario);
+
+        when(itemRepository.findById(item.getId())).thenReturn(Optional.of(item));
+
+        ItemUpdateRequestDTO itemUpdateDTO = new ItemUpdateRequestDTO("Carro","perfeito estado",
+                "Veículo",new BigDecimal("20.00"));
+
+        //Act
+        ItemEmLeilaoException exception = assertThrows(ItemEmLeilaoException.class,()->itemService.atualizarItem(item.getId(), itemUpdateDTO));
+
+        //Assert
+        assertEquals("Um item em leilão não pode ser editado", exception.getMessage());
+
+        verify(itemRepository,never()).save(any(Item.class));
+        verify(itemRepository).findById(item.getId());
+    }
+
+    @Test
+    void lancarExcecaoQuandoTentarAtualizarItemVendido()
+    {
+        //Arrange
+        Usuario proprietario = UsuarioFactory.criarUsuarioPronto();
+        Item item = ItemFactory.criarItemPersonalizado(1L,"Iphone","perfeito estado","telefone"
+                ,StatusItem.VENDIDO ,proprietario);
+
+        when(itemRepository.findById(item.getId())).thenReturn(Optional.of(item));
+
+        ItemUpdateRequestDTO itemUpdateDTO = new ItemUpdateRequestDTO("Carro","perfeito estado",
+                "Veículo",new BigDecimal("20.00"));
+
+        //Act
+        ItemVendidoException exception = assertThrows(ItemVendidoException.class,()->itemService.atualizarItem(item.getId(), itemUpdateDTO));
+
+        //Assert
+        assertEquals("Item vendido não pode ser editado", exception.getMessage());
+
+        verify(itemRepository,never()).save(any(Item.class));
+        verify(itemRepository).findById(item.getId());
+    }
+
+    @Test
+    void lancarExcecaoQuandoIdProprietarioInexistente()
+    {
+
+        //Arrange
+        Usuario proprietario = UsuarioFactory.criarUsuarioPronto();
+        Item item = ItemFactory.criarItemPronto(proprietario);
+
+        when(itemRepository.findById(item.getId())).thenReturn(Optional.empty());
+
+
+        ItemUpdateRequestDTO itemUpdateDTO = new ItemUpdateRequestDTO("Carro","perfeito estado",
+                "Veículo",new BigDecimal("20.00"));
+
+        //Act
+        IdNaoEncontradoException exception = assertThrows(IdNaoEncontradoException.class,()->itemService.atualizarItem(item.getId(), itemUpdateDTO));
+
+        //Assert
+        assertEquals("Id de item não encontrado", exception.getMessage());
+
+        verify(itemRepository,never()).save(any(Item.class));
+        verify(itemRepository).findById(item.getId());
+    }
+    
 
     // --- METODO AUXILIAR ---
 
