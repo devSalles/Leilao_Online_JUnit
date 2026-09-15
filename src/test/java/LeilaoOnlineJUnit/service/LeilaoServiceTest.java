0a6997ad267acf03d436caf68d795b5e9a164562
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-09-15 17:59:43 -0300

    leilão service test: início de implementação de metodo para realizar o lançamento de exceção caso item esteja vínculado a outro leilão

--- src/test/java/LeilaoOnlineJUnit/service/LeilaoServiceTest.java
+++ src/test/java/LeilaoOnlineJUnit/service/LeilaoServiceTest.java
@@ -153,6 +153,50 @@ public class LeilaoServiceTest {
         verify(leilaoRepository).save(any(Leilao.class));
     }
 
+//    @Test
+//    void deveLancarExcecaoQuandoItemEstiverVinculadoAOutroLeilao()
+//    {
+//        Usuario criador = UsuarioFactory.criarUsuarioPronto();
+//        Item item = ItemFactory.criarItemPronto(criador);
+//
+//        Leilao leilao = LeilaoFactory.criarLeilaoPersonalizado(
+//                1L,
+//                LocalDateTime.now().plusDays(1),
+//                LocalDateTime.now().plusDays(2),
+//                StatusLeilao.AGENDADO,
+//                item,
+//                criador
+//        );
+//
+//        LeilaoRequestDTO leilaoRequestDTO = new LeilaoRequestDTO(
+//                LocalDateTime.now().plusDays(1),
+//                LocalDateTime.now().plusDays(2),
+//                item.getId(),
+//                criador.getId()
+//        );
+//
+//        when(leilaoRepository.findById(1L)).thenReturn(Optional.of(leilao));
+//        when(itemService.buscarID(item.getId())).thenReturn(item);
+//        when(usuarioService.buscarIdUsuario(criador.getId())).thenReturn(criador);
+//
+//        when(leilaoRepository.existsByItemIdAndStatusLeilaoIn(
+//                eq(item.getId()),
+//                anyList()
+//        )).thenReturn(true);
+//
+//        assertThrows(
+//                ItemVinculadoAoLeilaoException.class,
+//                () -> leilaoService.atualizarLeilao(1L, leilaoRequestDTO)
+//        );
+//
+//        verify(leilaoRepository).existsByItemIdAndStatusLeilaoIn(
+//                eq(item.getId()),
+//                anyList()
+//        );
+//
+//        verify(leilaoRepository, never()).save(any(Leilao.class));
+//    }
+
     @Test
     void LancarExcecaoQuandoStatusDeLeilaoForDiferenteDeAgendado() {
         Usuario criador = UsuarioFactory.criarUsuarioPersonalizado(1L, "Bernardo", "28784851066", StatusUsuario.ATIVO);
