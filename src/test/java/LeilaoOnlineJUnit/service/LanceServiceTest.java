d95784c02656ed609447c1d03324be4eb2c53e56
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2026-09-20 15:47:49 -0300

    lance service test: criado metodo de busca por ID e lançamentro de exceção

--- src/test/java/LeilaoOnlineJUnit/service/LanceServiceTest.java
+++ src/test/java/LeilaoOnlineJUnit/service/LanceServiceTest.java
@@ -1,4 +1,69 @@
 package LeilaoOnlineJUnit.service;
 
+import LeilaoOnlineJUnit.dto.lance.LanceResponseDTO;
+import LeilaoOnlineJUnit.entity.Item;
+import LeilaoOnlineJUnit.entity.Lance;
+import LeilaoOnlineJUnit.entity.Leilao;
+import LeilaoOnlineJUnit.entity.Usuario;
+import LeilaoOnlineJUnit.factory.ItemFactory;
+import LeilaoOnlineJUnit.factory.LanceFactory;
+import LeilaoOnlineJUnit.factory.LeilaoFactory;
+import LeilaoOnlineJUnit.factory.UsuarioFactory;
+import LeilaoOnlineJUnit.infra.exception.IdNaoEncontradoException;
+import LeilaoOnlineJUnit.repository.LanceRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class LanceServiceTest {
+
+    @Mock
+    UsuarioService usuarioService;
+
+    @Mock
+    LanceRepository lanceRepository;
+
+    @Mock
+    LanceRepository leilaoRepository;
+
+    @InjectMocks
+    LanceService lanceService;
+
+
+    @Test
+    void deveBuscarLancePorIdComSucesso()
+    {
+        Usuario usuario = UsuarioFactory.criarUsuarioPronto();
+        Item item = ItemFactory.criarItemPronto(usuario);
+        Leilao leilao = LeilaoFactory.criarLeilaoPronto(item, usuario);
+        Lance lance = LanceFactory.criarLancePronto(usuario, leilao);
+        when(lanceRepository.findById(1L)).thenReturn(Optional.of(lance));
+
+
+        LanceResponseDTO resultado = lanceService.buscarLance(1L);
+
+        assertEquals(LanceResponseDTO.fromLance(lance), resultado);
+        verify(lanceRepository).findById(1L);
+    }
+
+    @Test
+    void deveLancarExcecaoQuandoLanceNaoEncontradoPorId()
+    {
+        when(lanceRepository.findById(99L)).thenReturn(Optional.empty());
+
+        IdNaoEncontradoException excecao = assertThrows(IdNaoEncontradoException.class, () -> lanceService.buscarLance(99L));
+
+        assertEquals("ID de lance não encontrado", excecao.getMessage());
+        verify(lanceRepository).findById(99L);
+    }
 }
